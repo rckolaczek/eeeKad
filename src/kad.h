29f91cff23c07d5c113865914365d9d7e785bc53
rckolaczek <28630584+rckolaczek@users.noreply.github.com>
2026-09-25 11:23:52 -0400

    Kad v0.3.0

--- src/kad.h
+++ src/kad.h
@@ -1,7 +1,7 @@
 #include "program_help.h"
 
 static const char KAD_NAME[] = "Kad";
-static const char KAD_VERSION[] = "0.1";
+static const char KAD_VERSION[] = "0.3.0";
 static const char KAD_REPOSITORY[] = "https://github.com/AmanoTeam/Kad";
 
 static const char KAD_DESCRIPTION[] = PROGRAM_HELP;
