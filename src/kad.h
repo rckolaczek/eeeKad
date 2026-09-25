b4c97bbc5d913ca442f5f335cc54ac8e72f8af8d
rckolaczek <28630584+rckolaczek@users.noreply.github.com>
2026-09-25 18:07:50 -0400

    Bump KAD_VERSION to 0.3.2 (runtime --version string)

--- src/kad.h
+++ src/kad.h
@@ -1,7 +1,7 @@
 #include "program_help.h"
 
 static const char KAD_NAME[] = "Kad";
-static const char KAD_VERSION[] = "0.3.1";
+static const char KAD_VERSION[] = "0.3.2";
 static const char KAD_REPOSITORY[] = "https://github.com/rckolaczek/eeeKad";
 
 static const char KAD_DESCRIPTION[] = PROGRAM_HELP;
